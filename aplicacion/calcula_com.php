3e639b441eaa25d264989292f828bb0746c80440
robefernandez22 <robertofernandezromero61@gmail.com>
2019-03-06 19:48:46 +0100

    practca 5

---
+++ aplicacion/calcula_com.php
@@ -0,0 +1,27 @@
+<?php
+
+	class Calcula { 
+
+		/** 
+		 * suma dos numeros
+		 * @param float $a 
+		 * @param float $b 
+		 * @return float
+		*/
+		public function suma($a, $b){ 
+			return $a+$b;
+		}
+
+		/** 
+		 * resta dos numeros
+		 * @param float $a 
+		 * @param float $b 
+		 * @return float 
+		*/ 
+		public function resta($a, $b){
+			return $a-$b; 
+		}
+
+	}
+
+?>
